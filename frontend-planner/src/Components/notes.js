c9c35e4f3a872f25ed070b527d222decb9b84959
sgaio <50683085+sgaio@users.noreply.github.com>
2019-10-14 20:34:07 -0700

    Add files via upload
    changes to grocery list

--- frontend-planner/src/Components/notes.js
+++ frontend-planner/src/Components/notes.js
@@ -4,13 +4,13 @@ import styled from "styled-components";
 const NotesBox = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: top;
   line-height:  ${props => props.lineHeight || "1px"};
   height: ${props => props.height || "500px"};
-  width: ${props => props.width || "300px"};
+  width: ${props => props.width || "400px"};
   background: rgba(0, 180, 180, 0.8);
   border-radius: ${props => props.borderRadius || "30px"};
-  padding: ${props => props.padding || "0px"}
+  padding: ${props => props.padding || "5px"}
   border: 5px;
   border-color: grey;
   border-style: solid;
@@ -25,8 +25,9 @@ class Notes extends Component {
         note: "" 
     }
 
+
     handleSubmit = (event) => {
-        // event.preventDefault();
+        event.preventDefault();
         let newNote ={
             note: this.state.note
         }
@@ -39,33 +40,55 @@ class Notes extends Component {
                 'Accept': 'application/json'
               }
           }).then((response) => {
-            // return response.json();
+            //return response.json();
           }).then((data) => {
             console.log("Completed");
+            window.location.reload();
           });
-            // // On success, run the following code
-            // .then(function() {
-        
-            //    alert("yay");
-        
-            // });
+    
     }
+    handleDelete = (id) => {
+        console.log("id", id);
+        fetch("http://localhost:8080/api/delete", {
+            body: JSON.stringify({ id: id }),
+            method: "POST",
+            headers: { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              }
+          }).then((response) => {
+             //return response.json();
+          }).then((data) => {
+            console.log("Completed");
+            window.location.reload();
+          });
+    }
+
     render () {
         let {plannerNotes} = this.props;
         return (
             <NotesBox>
                 <form >
-                    <label>new note</label>
+                    <h1>Grocery List</h1>
+                    <label>Enter Here</label>
                     <input id="newNotes" type="text" value = {this.state.note} onChange = {e=>{this.setState({note:e.target.value})}}></input>
-                    <button id="noteButton" class="btn btn-primary" onClick={this.handleSubmit}>submit</button>
+                    <button id="noteButton" class="btn btn-primary" onClick= {this.handleSubmit}>submit</button>
                 </form>
-                {plannerNotes.map(note=>{
+                
+                {
+                plannerNotes.length ? 
+                    plannerNotes.map(note=>{
                     return(
                         <h5 key = {note.id}>
                             {note.note}
+                            <button onClick={(event) => {event.preventDefault(); this.handleDelete(note.id)}}>Delete</button>
                         </h5>
-                    )
-                })}
+                    )})
+                    :
+                        <div>
+                            You don't have any notes
+                        </div>
+                }
 
                 
             </NotesBox>
